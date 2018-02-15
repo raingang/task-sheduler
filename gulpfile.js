60eb448a85fefff34c02a6f3741ee45bbe89ed8b
raingang <raingang4@gmail.com>
2018-02-15 22:17:44 +0200

    Create initial structure of app

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp'),
+    webpack = require('webpack-stream'),
+    browserSync = require('browser-sync'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify');
+
+gulp.task('webpack', function () {
+    return gulp.src([
+            './src/app/**/*.js'
+        ])
+        .pipe(webpack({
+            module: {
+                loaders: [{
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        presets: ['es2015', 'react']
+                    }
+                }]
+            },
+            output: {
+                filename: 'app.js'
+            }
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./src/public'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function () {
+    return gulp.src([
+            './src/scss/**/*.scss'
+        ])
+        .pipe(sass())
+        .pipe(gulp.dest('./src/public/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve',['webpack', 'sass'], function () {
+    browserSync.init({
+        server: './src'
+    });
+
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/app/**/*.js', ['webpack']);
+    gulp.watch('./src/index.html').on('change', browserSync.reload)
+});
+
+gulp.task('default', ['serve']);
---
+++ src/app/main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Hello world</h1>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
